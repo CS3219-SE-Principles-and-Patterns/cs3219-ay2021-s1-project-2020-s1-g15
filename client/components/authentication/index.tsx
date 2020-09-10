73cf4b078222382610bf58eadc5ba28afb9eea06
eugene teu <eugeneteu@gmail.com>
2020-09-10 16:18:39 +0800

    Add routing and authentication skeleton code

---
+++ client/components/authentication/index.tsx
@@ -0,0 +1,78 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+//@ts-nocheck
+import React, { createContext, useState, useContext, useEffect } from 'react'
+import Router, { useRouter } from 'next/router'
+
+type AuthContextType = {
+  user: {}
+  isAuthenticated: boolean
+  loading: boolean
+  login?: Promise<void>
+  logout?: Promise<void>
+}
+
+const AuthContext = createContext<AuthContextType>({
+  user: {},
+  isAuthenticated: false,
+  loading: false,
+})
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function loadUserFromCookies() {
+      setLoading(false)
+    }
+    loadUserFromCookies()
+  }, [])
+
+  const login = async (email, password) => {
+    /*
+    const { data: token } = await api.post('auth/login', { email, password })
+
+    if (token) {
+      console.log('Got token')
+      Cookies.set('token', token, { expires: 60 })
+      api.defaults.headers.Authorization = `Bearer ${token.token}`
+      const { data: user } = await api.get('users/me')
+      setUser(user)
+      console.log('Got user', user)
+    }*/
+  }
+
+  const logout = (email: string, password: string) => {
+    /*
+    Cookies.remove('token')
+    setUser(null)
+    window.location.pathname = '/login'*/
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{ isAuthenticated: !!user, user, login, loading, logout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default function useAuth() {
+  const context = useContext(AuthContext)
+
+  return context
+}
+
+export function ProtectRoute({ Component: Component, ...rest }) {
+  return () => {
+    const { user, isAuthenticated, loading } = useAuth()
+    const router = useRouter()
+
+    useEffect(() => {
+      if (!isAuthenticated && !loading) Router.push('/login')
+    }, [loading, isAuthenticated])
+
+    return <Component {...rest} />
+  }
+}
---
+++ client/pages/forum/index.tsx
@@ -0,0 +1,7 @@
+import FluidPage from '../../components/layout'
+
+const Forum = (): JSX.Element => {
+  return <FluidPage>{<p>I am forum</p>}</FluidPage>
+}
+
+export default Forum
