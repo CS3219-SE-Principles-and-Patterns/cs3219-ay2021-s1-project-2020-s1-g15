671ebf31e3a6da24df78aeae75190e82ed7f79c8
Aaron Choo <choozy.aaron@gmail.com>
2020-10-09 20:41:48 +0800

    fix: use fs instead of dynamic import to read admin key

--- server/src/services/authentication.ts
+++ server/src/services/authentication.ts
@@ -1,18 +1,38 @@
+import * as fs from "fs";
+import * as path from "path";
 import * as admin from "firebase-admin";
 
-async function initAuth(): Promise<void> {
-  // assume we are prod:
-  let credential = admin.credential.applicationDefault();
-
-  if (process.env.NODE_ENV === "dev") {
-    const serviceAccount = await import("../firebase-adminsdk.json");
-
-    credential = admin.credential.cert({
-      privateKey: serviceAccount.private_key,
-      clientEmail: serviceAccount.client_email,
-      projectId: serviceAccount.project_id,
-    });
+/**
+ * Returns the Firebase Admin credentials for local development.
+ *
+ * Requires the "firebase-adminsdk.json" secret to be located within
+ * the /server directory.
+ */
+async function getServiceAccountCredential(): Promise<
+  admin.credential.Credential
+> {
+  if (process.env.NODE_ENV !== "dev") {
+    throw new Error(
+      "This function should only be called when developing locally!"
+    );
   }
+
+  const pathToSdk = path.join(__dirname, "..", "..", "firebase-adminsdk.json");
+  const jsonFile = fs.readFileSync(pathToSdk, "utf8");
+  const serviceAccount = JSON.parse(jsonFile);
+
+  return admin.credential.cert({
+    privateKey: serviceAccount.private_key,
+    clientEmail: serviceAccount.client_email,
+    projectId: serviceAccount.project_id,
+  });
+}
+
+async function initAuth(): Promise<void> {
+  const credential =
+    process.env.NODE_ENV === "dev"
+      ? await getServiceAccountCredential()
+      : admin.credential.applicationDefault();
 
   admin.initializeApp({
     credential,
