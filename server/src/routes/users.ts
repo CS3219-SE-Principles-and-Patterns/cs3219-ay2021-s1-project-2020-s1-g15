3f9a0d4877a3ede6eaebfda41b7d65d949ee5fd5
Aaron Choo <choozy.aaron@gmail.com>
2020-10-12 17:16:54 +0800

    refactor: shift error handling from users route to controller

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,32 +1,18 @@
 import { Router, Request, Response } from "express";
 
 import { User } from "../models";
-import { HttpStatusCode, ApiError, ApiErrorMessage } from "../utils";
+import { HttpStatusCode, UserRequestBody } from "../utils";
 import { createUser } from "../controllers/users";
 
 const router: Router = Router();
 
 router.post("/register", async (req: Request, res: Response) => {
-  const email: string | undefined = req.body.email;
-  const password: string | undefined = req.body.password;
+  const data: UserRequestBody = {
+    email: req.body.email,
+    password: req.body.password,
+  };
 
-  if (!email || !password) {
-    throw new ApiError(
-      HttpStatusCode.BAD_REQUEST,
-      ApiErrorMessage.User.MISSING_REQUIRED_FIELDS
-    );
-  }
-
-  const trimmedEmail: string = email.trim();
-  const trimmedPassword: string = password.trim();
-  if (trimmedPassword === "" || trimmedEmail === "") {
-    throw new ApiError(
-      HttpStatusCode.BAD_REQUEST,
-      ApiErrorMessage.User.INVALID_FIELDS
-    );
-  }
-
-  const createdUser: User = await createUser(trimmedEmail, trimmedPassword);
+  const createdUser: User = await createUser(data);
 
   return res.status(HttpStatusCode.OK).json(createdUser);
 });
