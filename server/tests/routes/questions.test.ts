b79fc176e5cd1c4cebdb44d71d24c596e3cde86c
Aaron Choo <choozy.aaron@gmail.com>
2020-10-12 16:15:27 +0800

    test: remove auth route tests from questions

--- server/tests/routes/questions.test.ts
+++ server/tests/routes/questions.test.ts
@@ -1,27 +1,29 @@
+/**
+ * Integration test for the `/api/questions` route. Only the prominent and happy
+ * path will be tested here. All error handling is tested on the questions controller.
+ */
 import request from "supertest";
 import { ObjectId } from "mongodb";
 
-import server from "../../src/services/server";
-import { initDb, closeDb } from "../../src/services/database";
-import { createQuestion } from "../../src/controllers/questions";
-import { HttpStatusCode, Level, Subject } from "../../src/utils";
+import server from "src/services/server";
+import {
+  initDb,
+  closeDb,
+  getAnswersCollection,
+  getQuestionsCollection,
+  getUsersCollection,
+} from "src/services/database";
+import { createQuestion } from "src/controllers/questions";
+import { HttpStatusCode, Level, Subject, QuestionRequestBody } from "src/utils";
 
 const API_ENDPOINT = "/api/questions";
 
-const INVALID_ID = "123";
-const VALID_ID_UNUSED = new ObjectId();
-const INVALID_DATA = { markdown: "" };
-const VALID_DATA_1 = {
-  title: "my title",
+const VALID_USER_ID = new ObjectId();
+const VALID_REQUEST_DATA: QuestionRequestBody = {
+  title: "This is the title!",
   markdown: "hello",
   level: Level.DEFAULT,
-  subject: Subject.BIOLOGY,
-};
-const VALID_DATA_2 = {
-  title: "my title",
-  markdown: "world",
-  level: Level.DEFAULT,
-  subject: Subject.SCIENCE,
+  subject: Subject.GENERAL,
 };
 
 beforeAll(async (done) => {
@@ -36,6 +38,14 @@ afterAll(async (done) => {
   done();
 });
 
+beforeEach(async (done) => {
+  // clear all docs from all collections before each test suite to prevent runs from interfering with one another
+  await getAnswersCollection().remove({});
+  await getQuestionsCollection().remove({});
+  await getUsersCollection().remove({});
+  done();
+});
+
 describe("GET request - list all questions", () => {
   it("should return 200", async (done) => {
     const res = await request(server).get(API_ENDPOINT).send();
@@ -47,32 +57,10 @@ describe("GET request - list all questions", () => {
 });
 
 describe("GET request - find single question by id", () => {
-  it("should return 400 if ObjectId is invalid", async (done) => {
-    const res = await request(server)
-      .get(`${API_ENDPOINT}/${INVALID_ID}`)
-      .send();
-
-    expect(res.status).toEqual(HttpStatusCode.BAD_REQUEST);
-    done();
-  });
-
-  it("should return 404 if question is not found", async (done) => {
-    const res = await request(server)
-      .get(`${API_ENDPOINT}/${VALID_ID_UNUSED}`)
-      .send();
-
-    expect(res.status).toEqual(HttpStatusCode.NOT_FOUND);
-    done();
-  });
-
   it("should return 200 and the question on success", async (done) => {
-    const { title, markdown, level, subject } = VALID_DATA_1;
     const createdQuestion = await createQuestion(
-      title,
-      markdown,
-      new ObjectId(),
-      level,
-      subject
+      VALID_USER_ID,
+      VALID_REQUEST_DATA
     );
 
     const res = await request(server)
@@ -80,133 +68,7 @@ describe("GET request - find single question by id", () => {
       .send();
 
     expect(res.status).toEqual(HttpStatusCode.OK);
-    // check if ID is the same:
-    expect(createdQuestion._id.equals(res.body._id)).toBe(true);
-    // check if markdown field is the same:
-    expect(res.body.markdown).toEqual(VALID_DATA_1.markdown);
-    done();
-  });
-});
-
-describe("POST request - create a question", () => {
-  it("should return 400 if any required fields are missing", async (done) => {
-    const res = await request(server).post(API_ENDPOINT).send();
-
-    expect(res.status).toEqual(HttpStatusCode.BAD_REQUEST);
-    done();
-  });
-
-  it("should return 400 if any fields are invalid", async (done) => {
-    const res = await request(server).post(API_ENDPOINT).send(INVALID_DATA);
-
-    expect(res.status).toEqual(HttpStatusCode.BAD_REQUEST);
-    done();
-  });
-
-  it("should return 201 and the created document on success", async (done) => {
-    const res = await request(server).post(API_ENDPOINT).send(VALID_DATA_1);
-
-    expect(res.status).toEqual(HttpStatusCode.CREATED);
-    expect(res.body.markdown).toEqual(VALID_DATA_1.markdown);
-    done();
-  });
-});
-
-describe("PUT request - update a single question", () => {
-  it("should return 400 if ObjectId is invalid", async (done) => {
-    const res = await request(server)
-      .put(`${API_ENDPOINT}/${INVALID_ID}`)
-      .send();
-
-    expect(res.status).toEqual(HttpStatusCode.BAD_REQUEST);
-    done();
-  });
-
-  it("should return 400 if any required fields are missing", async (done) => {
-    const res = await request(server)
-      .put(`${API_ENDPOINT}/${VALID_ID_UNUSED}`)
-      .send();
-
-    expect(res.status).toEqual(HttpStatusCode.BAD_REQUEST);
-    done();
-  });
-
-  it("should return 400 if any fields are invalid", async (done) => {
-    const res = await request(server)
-      .put(`${API_ENDPOINT}/${VALID_ID_UNUSED}`)
-      .send(INVALID_DATA);
-
-    expect(res.status).toEqual(HttpStatusCode.BAD_REQUEST);
-    done();
-  });
-
-  it("should return 404 if question is not found", async (done) => {
-    const res = await request(server)
-      .put(`${API_ENDPOINT}/${VALID_ID_UNUSED}`)
-      .send(VALID_DATA_1);
-
-    expect(res.status).toEqual(HttpStatusCode.NOT_FOUND);
-    done();
-  });
-
-  it("should return 200 and the updated question on success", async (done) => {
-    const { title, markdown, level, subject } = VALID_DATA_1;
-    const createdQuestion = await createQuestion(
-      title,
-      markdown,
-      new ObjectId(),
-      level,
-      subject
-    );
-
-    const res = await request(server)
-      .put(`${API_ENDPOINT}/${createdQuestion._id}`)
-      .send(VALID_DATA_2);
-
-    expect(res.status).toEqual(HttpStatusCode.OK);
-    // check if ID is the same:
-    expect(createdQuestion._id.equals(res.body._id)).toBe(true);
-    // check if markdown field is updated:
-    expect(res.body.markdown).toEqual(VALID_DATA_2.markdown);
-    done();
-  });
-});
-
-describe("DELETE request", () => {
-  it("should return 400 if ObjectId is invalid", async (done) => {
-    const res = await request(server)
-      .delete(`${API_ENDPOINT}/${INVALID_ID}`)
-      .send();
-
-    expect(res.status).toEqual(HttpStatusCode.BAD_REQUEST);
-    done();
-  });
-
-  it("should return 404 if question is not found", async (done) => {
-    const res = await request(server)
-      .delete(`${API_ENDPOINT}/${VALID_ID_UNUSED}`)
-      .send();
-
-    expect(res.status).toEqual(HttpStatusCode.NOT_FOUND);
-    done();
-  });
-
-  it("should return 204 on success", async (done) => {
-    const { title, markdown, level, subject } = VALID_DATA_1;
-    const createdQuestion = await createQuestion(
-      title,
-      markdown,
-      new ObjectId(),
-      level,
-      subject
-    );
-
-    const res = await request(server)
-      .delete(`${API_ENDPOINT}/${createdQuestion._id}`)
-      .send();
-
-    expect(res.status).toEqual(HttpStatusCode.NO_CONTENT);
-    expect(res.body).toMatchObject({});
+    expect(res.body._id).toBe(createdQuestion._id.toHexString());
     done();
   });
 });
