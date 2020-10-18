d90541e9bc7718f7bec1ef45359c1c2e27f7fcee
Aaron Choo <choozy.aaron@gmail.com>
2020-10-18 14:42:02 +0800

    fix: get question on next server for seo

--- client/pages/question/[qid]/index.tsx
+++ client/pages/question/[qid]/index.tsx
@@ -1,63 +1,39 @@
-import React, { FC, useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import { Spin } from "antd";
+import React, { FC } from "react";
+import { GetServerSideProps } from "next";
 
 import { getSingleQuestion } from "components/api";
 import FluidPage from "components/layout";
-import { listOfAnswersMock, pageTitles, Question } from "util/index";
+import { Question, Answer } from "util/index";
 import ViewQuestion from "components/questions/view-question";
 
 type QuestionsProps = {
-  query: string;
+  question: Question;
+  answers: Answer[];
 };
 
-/**
- * A single Question Page
- * @param query : qid question id. Sent via url params
- */
-const Questions: FC<QuestionsProps> = ({ query }): JSX.Element => {
-  const router = useRouter();
-  const qid = router.query.qid as string;
-
-  const [question, setQuestion] = useState<Question | undefined>(undefined);
-  const [loading, setLoading] = useState<boolean>(false);
-
-  useEffect(() => {
-    const fetchQuestion = async (id: string) => {
-      setLoading(true);
-      try {
-        const question = await getSingleQuestion(id);
-        setQuestion(question);
-      } catch (err) {
-        console.log(err);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchQuestion(qid);
-  }, [qid]);
-
+const Questions: FC<QuestionsProps> = ({ question, answers }): JSX.Element => {
   return (
-    <FluidPage title={pageTitles.question}>
-      <Spin spinning={loading}>
-        {question ? (
-          <ViewQuestion question={question} answers={listOfAnswersMock} />
-        ) : (
-          <></>
-        )}
-      </Spin>
+    <FluidPage title={question.title}>
+      <ViewQuestion question={question} answers={answers} />
     </FluidPage>
   );
 };
-// TODO: ssr this page
-// This gets called on every request
-export async function getServerSideProps() {
-  // Fetch data from external API
-  //const res = await fetch(`${process.env.localhost}questions/`)
-  //const data = await res.json()
-  const data = {};
-  // Pass data to the page via props
-  return { props: { data } };
-}
+
+// SSR: this gets called on every page request
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  if (params == null) {
+    return { props: {} };
+  }
+
+  const { qid } = params;
+  const question = await getSingleQuestion(qid as string);
+  const answers = []; // TODO: get actual answers
+  return {
+    props: {
+      question: question,
+      answers: answers,
+    },
+  };
+};
 
 export default Questions;
