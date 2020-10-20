46273aee89f4721bcafe3e222fea49b302aed097
Aaron Choo <choozy.aaron@gmail.com>
2020-10-20 11:14:25 +0800

    refactor: shift helper functions to correct dir

--- client/util/index.ts
+++ client/util/index.ts
@@ -1,6 +1,4 @@
 export * from "./constants";
 export * from "./types";
-export * from "./convertor";
+export * from "./helpers";
 export * from "./mock";
-export * from "./markdown-to-react-node";
-export * from "./date-utils";
