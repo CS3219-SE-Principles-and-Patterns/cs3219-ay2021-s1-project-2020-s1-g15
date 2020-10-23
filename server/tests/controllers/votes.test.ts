83ea5052c2bacaa2a0653301a8726b92360246b3
Aaron Choo <choozy.aaron@gmail.com>
2020-10-23 13:46:56 +0800

    test: add question vote test

--- server/tests/controllers/votes.test.ts
+++ server/tests/controllers/votes.test.ts
@@ -8,6 +8,28 @@ const VALID_USER_ID = new ObjectId();
 const VALID_QUESTION_ID = new ObjectId();
 const VALID_ANSWER_ID = new ObjectId();
 
+async function insertQuestionVote(
+  voteType: VoteType
+): Promise<VoteIncrementObject> {
+  return handleQuestionVote(
+    VALID_USER_ID,
+    VALID_QUESTION_ID,
+    VoteCommand.INSERT,
+    voteType
+  );
+}
+
+async function removeQuestionVote(
+  voteType: VoteType
+): Promise<VoteIncrementObject> {
+  return handleQuestionVote(
+    VALID_USER_ID,
+    VALID_QUESTION_ID,
+    VoteCommand.REMOVE,
+    voteType
+  );
+}
+
 async function insertAnswerVote(
   voteType: VoteType
 ): Promise<VoteIncrementObject> {
@@ -69,6 +91,170 @@ describe("Vote Creation", () => {
   });
 });
 
+// -------------
+// QUESTION VOTE
+// -------------
+
+describe("Insert brand new question vote", () => {
+  beforeEach(async (done) => {
+    // clear all docs from all collections before each test suite to prevent runs from interfering with one another
+    await getVotesCollection().deleteMany({});
+    done();
+  });
+
+  it("should create correct VoteIncrementObject for upvote", async () => {
+    const incrementObject = await insertQuestionVote(VoteType.UPVOTE);
+
+    expect(incrementObject.upvotes).toBe(1);
+    expect(incrementObject.downvotes).toBe(0);
+  });
+
+  it("should create correct VoteIncrementObject for downvote", async () => {
+    const incrementObject = await insertQuestionVote(VoteType.DOWNVOTE);
+
+    expect(incrementObject.upvotes).toBe(0);
+    expect(incrementObject.downvotes).toBe(1);
+  });
+});
+
+describe("Insert existing question vote of same type", () => {
+  beforeEach(async (done) => {
+    // clear all docs from all collections before each test suite to prevent runs from interfering with one another
+    await getVotesCollection().deleteMany({});
+    done();
+  });
+
+  it("should create correct VoteIncrementObject for upvote", async () => {
+    // upvote once:
+    await insertQuestionVote(VoteType.UPVOTE);
+    // upvote again:
+    const incrementObject = await insertQuestionVote(VoteType.UPVOTE);
+
+    expect(incrementObject.upvotes).toBe(0);
+    expect(incrementObject.downvotes).toBe(0);
+  });
+
+  it("should create correct VoteIncrementObject for downvote", async () => {
+    // downvote once:
+    await insertQuestionVote(VoteType.DOWNVOTE);
+    // downvote again:
+    const incrementObject = await insertQuestionVote(VoteType.DOWNVOTE);
+
+    expect(incrementObject.upvotes).toBe(0);
+    expect(incrementObject.downvotes).toBe(0);
+  });
+});
+
+describe("Insert existing question vote of different type", () => {
+  beforeEach(async (done) => {
+    // clear all docs from all collections before each test suite to prevent runs from interfering with one another
+    await getVotesCollection().deleteMany({});
+    done();
+  });
+
+  it("should create correct VoteIncrementObject for downvote after upvote", async () => {
+    // upvote first:
+    await insertQuestionVote(VoteType.UPVOTE);
+    // then downvote:
+    const incrementObject = await insertQuestionVote(VoteType.DOWNVOTE);
+
+    expect(incrementObject.upvotes).toBe(-1);
+    expect(incrementObject.downvotes).toBe(1);
+  });
+
+  it("should create correct VoteIncrementObject for upvote after downvote", async () => {
+    // downvote first:
+    await insertQuestionVote(VoteType.DOWNVOTE);
+    // then upvote:
+    const incrementObject = await insertQuestionVote(VoteType.UPVOTE);
+
+    expect(incrementObject.upvotes).toBe(1);
+    expect(incrementObject.downvotes).toBe(-1);
+  });
+});
+
+describe("Remove non-existent question vote", () => {
+  beforeEach(async (done) => {
+    // clear all docs from all collections before each test suite to prevent runs from interfering with one another
+    await getVotesCollection().deleteMany({});
+    done();
+  });
+
+  it("should create correct VoteIncrementObject for upvote", async () => {
+    const incrementObject = await removeQuestionVote(VoteType.UPVOTE);
+
+    expect(incrementObject.upvotes).toBe(0);
+    expect(incrementObject.downvotes).toBe(0);
+  });
+
+  it("should create correct VoteIncrementObject for downvote", async () => {
+    const incrementObject = await removeQuestionVote(VoteType.DOWNVOTE);
+
+    expect(incrementObject.upvotes).toBe(0);
+    expect(incrementObject.downvotes).toBe(0);
+  });
+});
+
+describe("Remove existing question vote of same type", () => {
+  beforeEach(async (done) => {
+    // clear all docs from all collections before each test suite to prevent runs from interfering with one another
+    await getVotesCollection().deleteMany({});
+    done();
+  });
+
+  it("should create correct VoteIncrementObject for upvote", async () => {
+    // upvote first:
+    await insertQuestionVote(VoteType.UPVOTE);
+    // then remove the upvote:
+    const incrementObject = await removeQuestionVote(VoteType.UPVOTE);
+
+    expect(incrementObject.upvotes).toBe(-1);
+    expect(incrementObject.downvotes).toBe(0);
+  });
+
+  it("should create correct VoteIncrementObject for downvote", async () => {
+    // downvote first:
+    await insertQuestionVote(VoteType.DOWNVOTE);
+    // then remove the downvote:
+    const incrementObject = await removeQuestionVote(VoteType.DOWNVOTE);
+
+    expect(incrementObject.upvotes).toBe(0);
+    expect(incrementObject.downvotes).toBe(-1);
+  });
+});
+
+describe("Remove existing question vote of different type", () => {
+  beforeEach(async (done) => {
+    // clear all docs from all collections before each test suite to prevent runs from interfering with one another
+    await getVotesCollection().deleteMany({});
+    done();
+  });
+
+  it("should create correct VoteIncrementObject for removing non-existent downvote", async () => {
+    // upvote first:
+    await insertQuestionVote(VoteType.UPVOTE);
+    // then remove non existent downvote:
+    const incrementObject = await removeQuestionVote(VoteType.DOWNVOTE);
+
+    expect(incrementObject.upvotes).toBe(0);
+    expect(incrementObject.downvotes).toBe(0);
+  });
+
+  it("should create correct VoteIncrementObject for removing non-existent upvote", async () => {
+    // downvote first:
+    await insertQuestionVote(VoteType.DOWNVOTE);
+    // then remove non existent upvote:
+    const incrementObject = await removeQuestionVote(VoteType.UPVOTE);
+
+    expect(incrementObject.upvotes).toBe(0);
+    expect(incrementObject.downvotes).toBe(0);
+  });
+});
+
+// -------------
+// ANSWER VOTE
+// -------------
+
 describe("Insert brand new answer vote", () => {
   beforeEach(async (done) => {
     // clear all docs from all collections before each test suite to prevent runs from interfering with one another
@@ -119,7 +305,7 @@ describe("Insert existing answer vote of same type", () => {
   });
 });
 
-describe("Insert existing vote of different type", () => {
+describe("Insert existing answer vote of different type", () => {
   beforeEach(async (done) => {
     // clear all docs from all collections before each test suite to prevent runs from interfering with one another
     await getVotesCollection().deleteMany({});
@@ -147,7 +333,7 @@ describe("Insert existing vote of different type", () => {
   });
 });
 
-describe("Remove non-existent vote", () => {
+describe("Remove non-existent answer vote", () => {
   beforeEach(async (done) => {
     // clear all docs from all collections before each test suite to prevent runs from interfering with one another
     await getVotesCollection().deleteMany({});
@@ -169,7 +355,7 @@ describe("Remove non-existent vote", () => {
   });
 });
 
-describe("Remove existing vote of same type", () => {
+describe("Remove existing answer vote of same type", () => {
   beforeEach(async (done) => {
     // clear all docs from all collections before each test suite to prevent runs from interfering with one another
     await getVotesCollection().deleteMany({});
@@ -197,7 +383,7 @@ describe("Remove existing vote of same type", () => {
   });
 });
 
-describe("Remove existing vote of different type", () => {
+describe("Remove existing answer vote of different type", () => {
   beforeEach(async (done) => {
     // clear all docs from all collections before each test suite to prevent runs from interfering with one another
     await getVotesCollection().deleteMany({});
