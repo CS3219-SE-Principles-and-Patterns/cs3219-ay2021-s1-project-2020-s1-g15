896de406df85dfe173cb77d7b2e08b84fea0bcd3
Aaron Choo <choozy.aaron@gmail.com>
2020-11-10 22:11:00 +0800

    feat: tweak user page design

--- client/components/user/index.tsx
+++ client/components/user/index.tsx
@@ -1,209 +1,241 @@
 import React, { FC } from "react";
-import { Level, markdownToReactNode, Route, User } from "../../utils";
+import Link from "next/link";
 import {
   AlertOutlined,
-  LeftOutlined,
   QuestionCircleOutlined,
+  LikeOutlined,
+  DislikeOutlined,
 } from "@ant-design/icons";
 import {
-  PageHeader,
   Statistic,
-  Layout,
-  Divider,
   Tabs,
   List,
   Typography,
-  Tag,
-  Space,
-  Card,
   Row,
   Col,
+  Space,
+  Tag,
+  Button,
+  Divider,
 } from "antd";
 
-import styles from "./user.module.css";
-import Link from "next/link";
-import QuestionPreview from "components/questions/QuestionPreview";
-const { Title } = Typography;
+import {
+  markdownToReactNode,
+  Route,
+  User,
+  Question,
+  toRelativeTimeAgo,
+  Answer,
+} from "../../utils";
+
 const { TabPane } = Tabs;
+const { Text, Paragraph } = Typography;
 
 const userPageTabKeys = {
+  statistics: "statistics",
   questions: "questions",
   answers: "answers",
-  activity: "activity",
 };
 
-const { Content } = Layout;
 type ViewUserProps = {
   user: User;
 };
 
 const ViewUser: FC<ViewUserProps> = ({ user }) => {
+  const questions: Question[] = user.questions?.reverse() ?? [];
+  const answers: Answer[] = user.answers?.reverse() ?? [];
+  const topVotedQuestion: Question | null =
+    user.analytics?.topVotedQuestion ?? null;
+  const topVotedAnswer: Answer | null = user.analytics?.topVotedAnswer ?? null;
+
   const onChangeTab = (activeKey: string) => {
     console.log(activeKey);
   };
+
   return (
-    <>
-      <PageHeader
-        title={<h1>{user.username}</h1>}
-        subTitle="viewing user page"
-        backIcon={<LeftOutlined className={styles.iconOffset} size={64} />}
-        onBack={() => window.history.back()}
-      />
-      <Space style={{ width: "100%" }} direction="vertical" size="large">
-        <Content className={styles.mainContent}>
-          <h2>
-            <Typography.Text strong>Top Level</Typography.Text>
-          </h2>
-          <Row>
-            <Tag color="magenta">{Level.JUNIOR_COLLEGE}</Tag>
-          </Row>
-          <h2>
-            <Typography.Text strong>Top Subjects</Typography.Text>
-          </h2>
-          <Row>
-            <Tag color="magenta">{Level.PRIMARY}</Tag>
-          </Row>
-          <Divider />
-          <Tabs
-            defaultActiveKey={userPageTabKeys.questions}
-            onChange={onChangeTab}
-          >
-            <TabPane
-              forceRender
-              tab="Questions"
-              key={userPageTabKeys.questions}
-            >
-              <h2>
-                <Typography.Text strong>Questions</Typography.Text>
-              </h2>
-              <List
-                bordered
-                dataSource={user.questions}
-                renderItem={(item) => (
-                  <List.Item>
-                    <List.Item.Meta
-                      title={
-                        <>
-                          <Link
-                            href={`${Route.QUESTION}/[qid]/[slug]`}
-                            as={Route.QUESTION_VIEW(item._id, item.slug)}
-                          >
-                            {item.slug}
-                          </Link>
-                        </>
-                      }
-                      description={markdownToReactNode(item.markdown)}
-                    />
-                  </List.Item>
+    <Tabs
+      size="large"
+      defaultActiveKey={userPageTabKeys.statistics}
+      onChange={onChangeTab}
+    >
+      <TabPane forceRender tab="Statistics" key={userPageTabKeys.statistics}>
+        <Row gutter={[32, 16]}>
+          <Col>
+            <Statistic
+              title="Questions asked"
+              prefix={<QuestionCircleOutlined />}
+              value={user.analytics?.totalNumQuestions ?? 0}
+            />
+          </Col>
+          <Col>
+            <Statistic
+              title="Answers given"
+              prefix={<AlertOutlined />}
+              value={user.analytics?.totalNumAnswers ?? 0}
+            />
+          </Col>
+          <Col>
+            <Statistic
+              title="Questions asked per answer given"
+              value={user.analytics?.ratioQuestionsToAnswer.toFixed(2) ?? 0}
+            />
+          </Col>
+        </Row>
+        <Row gutter={[32, 0]}>
+          <Col>
+            <Statistic
+              title="Total upvotes"
+              prefix={<LikeOutlined />}
+              value={user.analytics?.totalNumUpvotes ?? 0}
+            />
+          </Col>
+          <Col>
+            <Statistic
+              title="Total downvotes"
+              prefix={<DislikeOutlined />}
+              value={user.analytics?.totalNumDownvotes ?? 0}
+            />
+          </Col>
+          <Col>
+            <Statistic
+              title="Upvotes per downvotes"
+              value={user.analytics?.ratioUpvotesToDownvotes.toFixed(2) ?? 0}
+            />
+          </Col>
+        </Row>
+
+        <Divider />
+        <Paragraph type="secondary">Top voted question</Paragraph>
+        {topVotedQuestion ? (
+          <Row style={{ width: "100%" }} justify="space-between" align="middle">
+            <Space direction="vertical">
+              <Row>
+                <Tag>
+                  <LikeOutlined /> {topVotedQuestion.upvotes}
+                </Tag>
+                <Tag>
+                  <DislikeOutlined /> {topVotedQuestion.downvotes}
+                </Tag>
+              </Row>
+              <div style={{ fontSize: "20px" }}>{topVotedQuestion.title}</div>
+              <Row>
+                <Tag color="geekblue">{topVotedQuestion.level}</Tag>
+                <Tag color="purple">{topVotedQuestion.subject}</Tag>
+              </Row>
+              <Text type="secondary">
+                {toRelativeTimeAgo(topVotedQuestion.createdAt)},{" "}
+                {topVotedQuestion.answerIds.length} answers
+              </Text>
+            </Space>
+            <Button type={"primary"}>
+              <Link
+                href={`${Route.QUESTION}/[qid]/[slug]`}
+                as={Route.QUESTION_VIEW(
+                  topVotedQuestion._id,
+                  topVotedQuestion.slug
                 )}
-              />
-            </TabPane>
-            <TabPane forceRender tab="Answers" key={userPageTabKeys.answers}>
-              <h2>
-                <Typography.Text strong>Answers</Typography.Text>
-              </h2>
-              <List
-                bordered
-                dataSource={user.answers}
-                renderItem={(item) => (
-                  <List.Item>
-                    <List.Item.Meta
-                      title={item.userId}
-                      description={markdownToReactNode(item.markdown)}
-                    />
-                  </List.Item>
-                )}
-              />
-            </TabPane>
-            <TabPane forceRender tab="Activity" key={userPageTabKeys.activity}>
-              <Card>
-                <Col>
-                  <Row gutter={[16, 16]}>
-                    <Col>
-                      <Statistic
-                        title="Questions Asked"
-                        prefix={<QuestionCircleOutlined />}
-                        value={user.analytics?.totalNumQuestions ?? 0}
-                      />
-                    </Col>
-                    <Col>
-                      <Statistic
-                        title="Answers Given"
-                        prefix={<AlertOutlined />}
-                        value={user.analytics?.totalNumAnswers ?? 0}
-                        style={{
-                          margin: "0 32px",
-                        }}
-                      />
-                    </Col>
+              >
+                View Question
+              </Link>
+            </Button>
+          </Row>
+        ) : (
+          <Paragraph type="secondary">No question asked yet...</Paragraph>
+        )}
+
+        <Divider />
+        <Paragraph type="secondary">Top voted answer</Paragraph>
+        {topVotedAnswer ? (
+          <Row>
+            <Space direction="vertical">
+              <Row>
+                <Tag>
+                  <LikeOutlined /> {topVotedAnswer.upvotes}
+                </Tag>
+                <Tag>
+                  <DislikeOutlined /> {topVotedAnswer.downvotes}
+                </Tag>
+              </Row>
+              <article className="markdown-body">
+                {markdownToReactNode(topVotedAnswer.markdown)}
+              </article>
+              <Text type="secondary">
+                {toRelativeTimeAgo(topVotedAnswer.createdAt)}
+              </Text>
+            </Space>
+          </Row>
+        ) : (
+          <Text type="secondary">No answer given yet...</Text>
+        )}
+      </TabPane>
+      <TabPane forceRender tab="Questions" key={userPageTabKeys.questions}>
+        <List
+          dataSource={questions}
+          renderItem={(question: Question) => (
+            <List.Item>
+              <Row
+                style={{ width: "100%" }}
+                justify="space-between"
+                align="middle"
+              >
+                <Space direction="vertical">
+                  <Row>
+                    <Tag>
+                      <LikeOutlined /> {question.upvotes}
+                    </Tag>
+                    <Tag>
+                      <DislikeOutlined /> {question.downvotes}
+                    </Tag>
                   </Row>
-                  <Row gutter={[16, 16]}>
-                    <Col>
-                      <Statistic
-                        title="Number of Upvotes"
-                        value={user.analytics?.totalNumUpvotes ?? 0}
-                      />
-                    </Col>
-                    <Col>
-                      <Statistic
-                        title="Number of Downvotes"
-                        value={user.analytics?.totalNumDownvotes ?? 0}
-                      />
-                    </Col>
+                  <div style={{ fontSize: "20px" }}>{question.title}</div>
+                  <Row>
+                    <Tag color="geekblue">{question.level}</Tag>
+                    <Tag color="purple">{question.subject}</Tag>
                   </Row>
-                  <Row gutter={[16, 16]}>
-                    <Col>
-                      <Statistic
-                        title="Questions To Answers"
-                        value={user.analytics?.ratioQuestionsToAnswer ?? 0}
-                      />
-                    </Col>
-                    <Col>
-                      <Statistic
-                        title="Upvotes to Downvotes"
-                        value={user.analytics?.ratioUpvotesToDownvotes ?? 0}
-                      />
-                    </Col>
-                  </Row>
-                  <Row gutter={[16, 16]}>
-                    <Col>
-                      <h2>
-                        <Typography.Text strong>
-                          Top Voted Answer
-                        </Typography.Text>
-                      </h2>
-                      {markdownToReactNode(
-                        user.analytics?.topVotedAnswer?.markdown ??
-                          "no answers upvoted"
-                      )}
-                    </Col>
-                  </Row>
-                  <Row gutter={[16, 16]}>
-                    <Col>
-                      <h2>
-                        <Typography.Text strong>
-                          Top Voted Question
-                        </Typography.Text>
-                      </h2>
-                      <QuestionPreview
-                        question={
-                          user.analytics?.topVotedQuestion ?? {
-                            title: "nil",
-                            markdown: "",
-                            subject: "nil",
-                            level: "nil",
-                          }
-                        }
-                      />
-                    </Col>
-                  </Row>
-                </Col>
-              </Card>
-            </TabPane>
-          </Tabs>
-        </Content>
-      </Space>
-    </>
+                  <Text type="secondary">
+                    {toRelativeTimeAgo(question.createdAt)},{" "}
+                    {question.answerIds.length} answers
+                  </Text>
+                </Space>
+                <Button type={"primary"}>
+                  <Link
+                    href={`${Route.QUESTION}/[qid]/[slug]`}
+                    as={Route.QUESTION_VIEW(question._id, question.slug)}
+                  >
+                    View Question
+                  </Link>
+                </Button>
+              </Row>
+            </List.Item>
+          )}
+        />
+      </TabPane>
+      <TabPane forceRender tab="Answers" key={userPageTabKeys.answers}>
+        <List
+          dataSource={answers}
+          renderItem={(answer: Answer) => (
+            <List.Item>
+              <Space direction="vertical">
+                <Row>
+                  <Tag>
+                    <LikeOutlined /> {answer.upvotes}
+                  </Tag>
+                  <Tag>
+                    <DislikeOutlined /> {answer.downvotes}
+                  </Tag>
+                </Row>
+                <article className="markdown-body">
+                  {markdownToReactNode(answer.markdown)}
+                </article>
+                <Text type="secondary">
+                  {toRelativeTimeAgo(answer.createdAt)}
+                </Text>
+              </Space>
+            </List.Item>
+          )}
+        />
+      </TabPane>
+    </Tabs>
   );
 };
 
